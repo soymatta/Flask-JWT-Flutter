1084d3238d6988670f828593ce4237b52da45b2d
Yassed Matta <yassed883@gmail.com>
2024-05-20 17:32:56 -0500

    ✨ | FEATURE: CORS allowed for all domains

--- db/db.py
+++ db/db.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from config import Config
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})  # Allow CORS for all domains
 app.template_folder = "../templates"
 
 app.config.from_object(Config)
